9df6bcbe8d2af27451d60dbfc091a26001bc7e60
Paulo Roberto <paulorantunes13@hotmail.com>
2025-08-11 14:21:34 -0300

    feat: Criando uma rota login para validação em memória.

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,6 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Eai pessoal!");
 
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+  if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+  {
+    return Results.Ok("Login com sucesso");
+  }
+  else
+  {
+    return Results.Unauthorized();
+  }
+});
+
+
+
+Console.WriteLine("http://localhost:5043");
 app.Run();
+
+
+public class LoginDTO {
+  public string Email { get; set; } = default!;
+  public string Senha { get; set; } = default!;
+
+} 
