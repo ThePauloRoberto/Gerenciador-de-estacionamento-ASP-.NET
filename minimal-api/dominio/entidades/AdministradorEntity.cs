752efe4971907d73791990366d109ed21834ca1e
Paulo Roberto <paulorantunes13@hotmail.com>
2025-08-12 13:30:53 -0300

    chore: Adicionando campos obrigatórios na entidade Administrador

--- minimal-api/dominio/entidades/AdministradorEntity.cs
+++ minimal-api/dominio/entidades/AdministradorEntity.cs
@@ -12,8 +12,12 @@ public class Administrador
   [Required]
   [StringLength(255)]
   public string Email { get; set; } = default!;
+
+  [Required]
   [StringLength(50)]
   public string Senha { get; set; } = default!;
+
+  [Required]
   [StringLength(10)]
   public string Perfil { get; set; } = default!;
 }
