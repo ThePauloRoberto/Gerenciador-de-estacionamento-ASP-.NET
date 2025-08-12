70a4fb9d723f19c12fb4c76da4a1b1b88c537c27
Paulo Roberto <paulorantunes13@hotmail.com>
2025-08-11 21:55:36 -0300

    feat: Criação da entidade Administrador e os usos dos decorators

---
+++ minimal-api/dominio/entidades/AdministradorEntity.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MinamalApi.Dominio.Entidades;
+
+public class Administrador
+{
+
+  [Key]
+  [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //Decorator de chave primária na entidade 
+  public string Id { get; set; } = default!;
+  [Required]
+  [StringLength(255)]
+  public string Email { get; set; } = default!;
+  [StringLength(50)]
+  public string Senha { get; set; } = default!;
+  [StringLength(10)]
+  public string Perfil { get; set; } = default!;
+}
