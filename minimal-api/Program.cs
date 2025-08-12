6b8dc44350af4b4e5a91bf4c57faa9e94da09488
Paulo Roberto <paulorantunes13@hotmail.com>
2025-08-12 14:29:42 -0300

    feat: Adicionando endpoint POST para criação de veiculos

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -7,9 +7,11 @@ using MinamalApi.Infraestrutura.DB;
 using MinimalApi.Dominio.Interfaces;
 using MinimalApi.DTOs;
 
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,12 +28,14 @@ builder.Services.AddDbContext<DbContexto>(
 );
 
 var app = builder.Build();
+#endregion
 
-
-
+#region Home
 app.MapGet("/", () => Results.Json(new Home()));
+#endregion 
 
-app.MapPost("/login", ([FromBody]LoginDTO loginDTO, IAdministradorServico administradorServico) =>
+#region Administradores
+app.MapPost("/Administradores/login", ([FromBody]LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
   if (administradorServico.Login(loginDTO) != null)
   {
@@ -42,10 +46,27 @@ app.MapPost("/login", ([FromBody]LoginDTO loginDTO, IAdministradorServico admini
     return Results.Unauthorized();
   }
 });
+#endregion
 
+#region Veiculos
+app.MapPost("/Veiculos", ([FromBody]VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
+  var veiculo = new Veiculo
+  {
+    Nome = veiculoDTO.Nome,
+    Marca = veiculoDTO.Marca,
+    Ano = veiculoDTO.Ano
+  };
+  veiculoServico.Incluir(veiculo);
 
+  return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
+});
+#endregion
+
+#region app
 app.UseSwagger();
 app.UseSwaggerUI();
 
 Console.WriteLine("http://localhost:5043");
 app.Run();
+#endregion
